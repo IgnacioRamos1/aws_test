bfbf4af6cda05b6d89050a44f9a4460095b73241
Nacho <ignacio.nahuel.ramos@gmail.com>
2023-02-22 02:54:30 -0300

    modified workflow

--- lambdas/testing/main.js
+++ lambdas/testing/main.js
@@ -6,7 +6,7 @@ exports.handler = function (event, context, callback) {
         headers: {
             'Content-Type': 'text/html; charset=utf-8',
         },
-        body: "Modified Lambda function!!",
+        body: "Modified Lambda function!",
     };
     callback(null, response);
 };
