c750f3cba08cc547c97f8928aff1ad7ffa33ec73
Valentin Vila <vilavalentin@gmail.com>
2023-02-23 02:13:09 +0100

    a

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,34 +1,8 @@
-every day companies grow from a simple
-app and one server
-to a larger infrastructure in a matter
-of an instant this can be bad news
-as it increases your weak points and
-attack surface keeping track of your
-security as you grow can be a slow and
-resource intensive task
-with hundreds of scanners and tools on
-the market
-faraday normalizes data from more than
-80 tools to empower your team to do
-meaningful work
-by integrating everything into a highly
-intuitive interface
-quickly gather manage and de-duplicate
-issues to unlock more time for discovery
-strategy and remediation
-our dashboard gives you a clear picture
-of how much and where
-you are especially vulnerable to
-integrate things further
-workflows and agents provide a flexible
-way to create rules
-and schedule your recurrent scans so you
-can keep an eye on your critical
-infrastructure
-at all times packed with a customizable
-reporting module and a powerful api
-faraday is all about shortening the gap
-from exposure to remediation
-join us in rethinking vulnerability
-management
-you
+resource "aws_api_gateway_rest_api" "example" {
+  name        = "ServerlessExample"
+  description = "Terraform Serverless Application Example"
+}
+
+output "base_url" {
+  value = aws_api_gateway_deployment.example.invoke_url
+}
