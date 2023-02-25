d857a662d0f6cb69faae2690dc8eec0db4de5e8b
Nacho <ignacio.nahuel.ramos@gmail.com>
2023-02-25 01:12:28 -0300

    api_gateway

--- terraform/environments/shared/lambdas/lambda_1/main.tf
+++ terraform/environments/shared/lambdas/lambda_1/main.tf
@@ -2,24 +2,12 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-resource "aws_iam_role" "lambda_exec" {
+data "aws_iam_role" "lambda_exec" {
   name = "lambda-exec-lambda_1"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
 }
 
 output "role_arn" {
-  value = aws_iam_role.lambda_exec.arn
+  value = data.aws_iam_role.lambda_exec.arn
 }
 
 module "lambda" {
@@ -28,5 +16,48 @@ module "lambda" {
   handler       = "main"
   runtime       = "go1.x"
   filename      = "../../../../../lambdas/testing2/main.zip"
-  role          = aws_iam_role.lambda_exec.arn
+  role          = data.aws_iam_role.lambda_exec.arn
+}
+
+
+# ------------------- API GATEWAY -------------------
+resource "aws_api_gateway_rest_api" "api_gateway" {
+  name = "my_api_gateway"
+}
+
+resource "aws_api_gateway_resource" "api_gateway_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = "test"
+}
+
+resource "aws_api_gateway_method" "api_gateway_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
+  resource_id   = aws_api_gateway_resource.api_gateway_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
+  resource_id             = aws_api_gateway_resource.api_gateway_resource.id
+  http_method             = "GET"
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = module.lambda.invoke_arn
+
+  request_parameters = {
+    "integration.request.header.X-Amz-Invocation-Type" = "'Event'"
+  }
+}
+
+resource "aws_api_gateway_deployment" "api_gateway_deployment" {
+  depends_on = [aws_api_gateway_integration.lambda_integration]
+
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  stage_name = "dev"
+}
+
+output "api_gateway_url" {
+  value = aws_api_gateway_deployment.api_gateway_deployment.invoke_url
 }
--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -9,3 +9,7 @@ resource "aws_lambda_function" "lambda_function" {
   filename = var.filename
   role = var.role
 }
+
+output "invoke_arn" {
+  value = aws_lambda_function.lambda_function.invoke_arn
+}
