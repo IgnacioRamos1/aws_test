0afdf96976d8bc713e75bcfce3eb13bc9083c2a4
Nacho <ignacio.nahuel.ramos@gmail.com>
2023-02-25 19:51:32 -0300

    api_gateway partial fix

--- terraform/environments/shared/lambdas/lambda_1/main.tf
+++ terraform/environments/shared/lambdas/lambda_1/main.tf
@@ -17,7 +17,7 @@ data "aws_s3_bucket" "lambda_bucket" {
   bucket = var.s3_bucket_name
 }
 
-resource "aws_s3_bucket_object" "lambda_zip" {
+resource "aws_s3_object" "lambda_zip" {
   bucket       = data.aws_s3_bucket.lambda_bucket.id
   key          = "${var.s3_key}/${var.function_name}.zip"
   source       = data.archive_file.lambda_zip.output_path
@@ -35,7 +35,7 @@ module "lambda" {
 }
 
 resource "aws_api_gateway_rest_api" "lambda_api" {
-  name = "lambda-api"
+  name = "olga-api"
 }
 
 resource "aws_api_gateway_resource" "lambda_resource" {
@@ -49,15 +49,29 @@ resource "aws_api_gateway_method" "lambda_method" {
   resource_id   = aws_api_gateway_resource.lambda_resource.id
   http_method   = "GET"
   authorization = "NONE"
-}
+} 
 
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.lambda_api.id
   resource_id             = aws_api_gateway_resource.lambda_resource.id
   http_method             = aws_api_gateway_method.lambda_method.http_method
   integration_http_method = "POST"
-  type                    = "AWS_PROXY"
+  type                    = "AWS"
   uri                     = module.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration_response" "integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.lambda_resource.id
+  http_method = aws_api_gateway_method.lambda_method.http_method
+  status_code = "200"  # o cualquier otro código de estado que quieras configurar
+}
+
+resource "aws_api_gateway_method_response" "method_response" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.lambda_resource.id
+  http_method = aws_api_gateway_method.lambda_method.http_method
+  status_code = "200"  # o cualquier otro código de estado que quieras configurar
 }
 
 resource "aws_lambda_permission" "lambda_permission" {
@@ -65,7 +79,15 @@ resource "aws_lambda_permission" "lambda_permission" {
   action        = "lambda:InvokeFunction"
   function_name =  module.lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "arn:aws:execute-api:sa-east-1:123456789012:${aws_api_gateway_rest_api.lambda_api.id}/*/*"
+  source_arn    = "arn:aws:execute-api:sa-east-1:123456789012:${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/*"
 }
 
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  stage_name  = "dev"
+}
+
+output "api_url" {
+  value = "https://${aws_api_gateway_rest_api.lambda_api.id}.execute-api.sa-east-1.amazonaws.com/dev/lambda"
+}
 
