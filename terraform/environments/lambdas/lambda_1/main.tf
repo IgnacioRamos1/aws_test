a4bef76ca8b8f0264fad99f4b8733811eeb974f6
Nacho <ignacio.nahuel.ramos@gmail.com>
2023-02-26 03:46:43 -0300

    deleted unnecesary files

---
+++ terraform/environments/lambdas/lambda_1/main.tf
@@ -0,0 +1,99 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+data "aws_iam_role" "lambda_exec" {
+  name = "lambda-exec"
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "../../../../lambdas/testing/main"
+  output_path = "/tmp/main.zip"
+}
+
+data "aws_s3_bucket" "lambda_bucket" {
+  bucket = var.s3_bucket_name
+}
+
+resource "aws_s3_object" "lambda_zip" {
+  bucket       = data.aws_s3_bucket.lambda_bucket.id
+  key          = "${var.s3_key}/${var.function_name}.zip"
+  source       = data.archive_file.lambda_zip.output_path
+  content_type = "application/zip"
+}
+
+module "lambda" {
+  source        = "../../../modules/lambda"
+  function_name = "lambda_1"
+  handler       = "main"
+  runtime       = "go1.x"
+  filename      = data.archive_file.lambda_zip.output_path
+  role          = data.aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_api_gateway_rest_api" "lambda_api" {
+  name = "olga-api"
+}
+
+resource "aws_api_gateway_resource" "lambda_resource" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  path_part   = "lambda"
+}
+
+resource "aws_api_gateway_method" "lambda_method" {
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  resource_id   = aws_api_gateway_resource.lambda_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+} 
+
+resource "aws_api_gateway_integration" "lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.lambda_api.id
+  resource_id             = aws_api_gateway_resource.lambda_resource.id
+  http_method             = aws_api_gateway_method.lambda_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = module.lambda.invoke_arn
+}
+
+resource "aws_api_gateway_integration_response" "integration_response" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.lambda_resource.id
+  http_method = aws_api_gateway_method.lambda_method.http_method
+  status_code = "200"  # o cualquier otro código de estado que quieras configurar
+}
+
+resource "aws_api_gateway_method_response" "method_response" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.lambda_resource.id
+  http_method = aws_api_gateway_method.lambda_method.http_method
+  status_code = "200"  # o cualquier otro código de estado que quieras configurar
+}
+
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name =  module.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.lambda_api.execution_arn}/*/*/*"
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_method.lambda_method,
+    aws_api_gateway_resource.lambda_resource,
+  ]
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  stage_name  = "dev"
+}
+
+output "api_url" {
+  value = "https://${aws_api_gateway_rest_api.lambda_api.id}.execute-api.sa-east-1.amazonaws.com/dev/lambda"
+}
+
---
+++ terraform/environments/lambdas/lambda_1/variables.tf
@@ -0,0 +1,34 @@
+variable "http_method" {
+  description = "The HTTP method for the API Gateway resource."
+  type        = string
+  default     = "GET"
+}
+
+variable "endpoint" {
+  description = "The endpoint for the API Gateway resource."
+  type        = string
+  default     = "lambda_1"
+}
+
+variable "stage_name" {
+  description = "The name of the stage to deploy the API Gateway resource to."
+  type        = string
+  default     = "dev"
+}
+variable s3_bucket_name {
+  description = "The name of the S3 bucket to store the API Gateway resource."
+  type        = string
+  default     = "olga-develop-bucket"
+}
+
+variable "s3_key" {
+  description = "The name of the S3 key to store the API Gateway resource."
+  type        = string
+  default     = "Go Binary"
+}
+
+variable "function_name" {
+  description = "The name of the Lambda function to invoke."
+  type        = string
+  default     = "lambda_1"
+}
